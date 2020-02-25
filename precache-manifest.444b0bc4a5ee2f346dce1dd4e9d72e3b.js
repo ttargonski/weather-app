8439dbec959ac2a052af1b992761cbaca4dadff6
Tomasz Targoński <tomasz.targonski95@gmail.com>
2020-02-25 23:43:05 +0100

    Updates

---
+++ precache-manifest.444b0bc4a5ee2f346dce1dd4e9d72e3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7248ca8b644c3a0ac32a5350a3f8c060",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "b29471b558e4b173089f",
+    "url": "/weather-app/static/css/main.5c296ba6.chunk.css"
+  },
+  {
+    "revision": "968bcd136d3116288f35",
+    "url": "/weather-app/static/js/2.8cd088cd.chunk.js"
+  },
+  {
+    "revision": "b29471b558e4b173089f",
+    "url": "/weather-app/static/js/main.d32eb7c1.chunk.js"
+  },
+  {
+    "revision": "20b7fe9aece0c845f972",
+    "url": "/weather-app/static/js/runtime-main.630a9fd3.js"
+  }
+]);
